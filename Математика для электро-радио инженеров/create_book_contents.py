3749cb4e07948624cec059d8747779eec070040b
dmitridev <42156228+dmitridev@users.noreply.github.com>
2022-06-01 13:49:26 +0300

    first commit

---
+++ Математика для электро-радио инженеров/create_book_contents.py
@@ -0,0 +1,38 @@
+import os
+
+def create_link(mainheader: str, subheader: str, name: str):
+    return '- [{0}]({1})'.format(name,os.path.join(mainheader.rstrip(), subheader.rstrip(),
+                           name.rstrip()).replace(' ', '%20') + '.md') 
+
+path = os.path.abspath('./')
+
+filename = 'Анго А. Математика для электро-и радиоинженеров.md'
+
+# теперь нужно обработать файл
+lines = []
+
+with open(filename, 'r+', encoding='utf-8') as file:
+    lines = [line.rstrip() for line in file.readlines()]
+
+    mainheader = ''
+    subheader = ''
+    name = ''
+
+    for i in range(0, len(lines)):
+        line = lines[i]
+        if (line.startswith('## ')):
+            mainheader = line[3:]
+
+        if (line.startswith('### ')):
+            subheader = line[4:]
+
+        if (line.startswith('-')):
+            name = line[2:]
+            line = create_link(mainheader, subheader, name)
+
+        line = line + '\n'
+        lines[i] = line
+        print(mainheader,subheader,name)
+
+    file.seek(0)
+    file.writelines(lines)
